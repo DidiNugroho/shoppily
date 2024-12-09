25ec5c1fcaacc4cce9be2cafc01fddda759ab77f
RenFrost <diidnugroho@gmail.com>
2024-12-09 18:51:16 +0700

    feat: added register functionality

--- shoppily-server/src/users/user.controller.ts
+++ shoppily-server/src/users/user.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { RegisterUserDto } from './dto/RegisterUser';
 
 @Controller('user')
 export class UserController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  async createUser() {}
+  @UsePipes(new ValidationPipe())
+  async registerUser(@Body() RegisterUserDto: RegisterUserDto) {
+    return this.usersService.registerUser(RegisterUserDto);
+  }
 }
--- shoppily-server/src/users/users.service.ts
+++ shoppily-server/src/users/users.service.ts
@@ -3,13 +3,19 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/User.schema';
 import { RegisterUserDto } from './dto/RegisterUser';
+import { hashPassword } from 'src/utils/bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  registerUser(registerUserDto: RegisterUserDto): Promise<User> {
-    const createdUser = new this.userModel(registerUserDto);
+  async registerUser(registerUserDto: RegisterUserDto): Promise<User> {
+    const hashedPassword = hashPassword(registerUserDto.password);
+    const createdUser = new this.userModel({
+      ...registerUserDto,
+      password: hashedPassword,
+    });
+
     return createdUser.save();
   }
 }
