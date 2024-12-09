3384cc89da5343f9c39683775fd9662fa49916c0
RenFrost <diidnugroho@gmail.com>
2024-12-09 20:38:04 +0700

    chore: added UserController to controllers in user module

--- shoppily-server/src/users/users.module.ts
+++ shoppily-server/src/users/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/User.schema';
 import { UsersService } from './users.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { UsersService } from './users.service';
       },
     ]),
   ],
+  controllers: [UserController],
   providers: [UsersService],
 })
 export class UsersModule {}
