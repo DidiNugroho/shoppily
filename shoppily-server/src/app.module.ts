7eaf1f6b719a7cbe0971bc7a34a66cf83ff5e538
RenFrost <diidnugroho@gmail.com>
2024-12-10 15:26:01 +0700

    chore: setting up redis

--- shoppily-server/src/app.module.ts
+++ shoppily-server/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { CacheModule, CacheStore } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -13,9 +15,21 @@ import { UsersModule } from './users/users.module';
       }),
       inject: [ConfigService],
     }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const store = await redisStore({
+          url: `redis://${configService.get<string>('REDIS_HOST')}:${configService.get<number>('REDIS_PORT')}`,
+        });
+        return {
+          store: store as unknown as CacheStore,
+          ttl: 5000,
+          max: 10,
+        };
+      },
+      inject: [ConfigService],
+    }),
     UsersModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
--- shoppily-server/src/users/users.service.ts
+++ shoppily-server/src/users/users.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/User.schema';
 import { RegisterUserDto } from './dto/RegisterUser';
-import { hashPassword } from 'src/utils/bcrypt';
+import { comparePassword, hashPassword } from 'src/utils/bcrypt';
 import { plainToInstance } from 'class-transformer';
 
 @Injectable()
@@ -20,5 +20,19 @@ export class UsersService {
     const savedUser = await createdUser.save();
 
     return plainToInstance(User, savedUser.toObject());
+  }
+
+  async loginUser(email: string, password: string): Promise<User> {
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    if (comparePassword(password, user.password)) {
+      throw new Error('Invalid password');
+    }
+
+    return plainToInstance(User, user.toObject());
   }
 }
